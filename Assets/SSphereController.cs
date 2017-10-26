b4c05a139ae69c2c6c691668f489dd4ab13f0f07
Masato Suzuki <nkc.Masato.Suzuki@gmail.com>
2017-10-26 10:39:06 +0900

    多分完成

---
+++ Assets/SSphereController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SSphereController : MonoBehaviour {
+
+    public void Shoot(Vector3 dir) {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision other) {
+        GetComponent<Rigidbody>().isKinematic = true;
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SSphereGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SSphereGenerator : MonoBehaviour {
+
+    public GameObject SSpherePrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)) {
+            GameObject SSphere = Instantiate(SSpherePrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            SSphere.GetComponent<SSphereController>().Shoot(worldDir.normalized * 2000);
+        }
+	}
+}
